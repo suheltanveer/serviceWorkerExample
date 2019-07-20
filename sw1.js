3156b4b2b35a9d31c81555506f7763e85b4db643
Suhel Tanveer <suhel.tanveer@gmail.com>
2019-07-20 09:21:56 +0530

    checking code for errors from original example

--- sw1.js
+++ sw1.js
@@ -1,43 +1,45 @@
-const cacheName = 'v1';
+const cacheName = 'v2';
 
 // Install Event
 self.addEventListener('install', (e) => {
     console.log(`%cSW: ${e.type}`, 'font-weight: bold; color: deeppink');
+    console.log('hello');
+
 })
 
 // Activate Event
 self.addEventListener('activate', e => {
     console.log(`%cSW: ${e.type}`, 'font-weight: bold; color: deeppink');
+    // Remove unwanted caches
     e.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cache => {
                     if (cache !== cacheName) {
                         console.log('%cSW :: Deleting Old Cache', 'color: darkorange');
-                        return caches.delete(cache)
+                        return caches.delete(cache);
                     }
                 })
-            )
+            );
         })
-    )
+    );
 })
 
 // Fetch Event
 self.addEventListener('fetch', e => {
     console.log('%cSW: ' + e.type, 'font-weight: bold; color: deeppink');
     e.respondWith(
-        fetch(e.request).then(
-            res => {
+        fetch(e.request)
+            .then(res => {
+                // Make copy/clone of response
                 const resClone = res.clone();
-                caches.open(cacheName)
-                    .then(cache => {
-                        cache.put(e.request, resClone);
-                    })
+                // Open cahce
+                caches.open(cacheName).then(cache => {
+                    // Add response to cache
+                    cache.put(e.request, resClone);
+                });
                 return res;
-            }
-        ).catch(err => {
-            caches.match(e.request)
-                .then(res => res)
-        })
-    )
+            })
+            .catch(err => caches.match(e.request).then(res => res))
+    );
 })
